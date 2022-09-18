9a7d9800b2583d66a62cbde9884b27e17b90adbb
vmdroidus <113832091+vmdroidus@users.noreply.github.com>
2022-09-18 18:43:57 +0700

    Add files via upload

---
+++ DockUbuntu18CrossOver.sh
@@ -0,0 +1,6 @@
+# Free Google novnc
+#!/bin/sh
+echo "Click Web Preview > Preview On Port 8080 to access novnc RDP"
+echo "Installing Ubuntu 18.04 Environment +  Chrome Web Browser + CrossOver ..."
+sudo docker run --name ubuntu18.04 --privileged --shm-size 1g -d -p 5900:5900 -p 8080:6080 son999/ubuntu-vnc
+seq 1 43200 | while read i; do echo -en "\r Running .     $i s /43200 s";sleep 0.1;echo -en "\r Running ..    $i s /43200 s";sleep 0.1;echo -en "\r Running ...   $i s /43200 s";sleep 0.1;echo -en "\r Running ....  $i s /43200 s";sleep 0.1;echo -en "\r Running ..... $i s /43200 s";sleep 0.1;echo -en "\r Running     . $i s /43200 s";sleep 0.1;echo -en "\r Running  .... $i s /43200 s";sleep 0.1;echo -en "\r Running   ... $i s /43200 s";sleep 0.1;echo -en "\r Running    .. $i s /43200 s";sleep 0.1;echo -en "\r Running     . $i s /43200 s";sleep 0.1; done
